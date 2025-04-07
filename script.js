f960fa6a23affa9f79728901114e821f31ace544
matheus.vigna <matheus.vigna@globalhitss.com.br>
2025-04-07 12:32:47 -0300

    changed the homepage and add the the pix page

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Página carregada! O formulário do Google Forms está pronto.");
 });
+
+function copiarPix() {
+    const campoPix = document.getElementById("chavePix");
+    campoPix.select();
+    campoPix.setSelectionRange(0, 99999); // para mobile
+
+    document.execCommand("copy");
+    alert("Chave Pix copiada com sucesso!");
+}
